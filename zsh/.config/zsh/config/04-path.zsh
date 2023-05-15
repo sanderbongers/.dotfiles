f563fc710397d4a99f25bf1fcbc4485789b4e0d4
Sander Bongers <sander@bonge.rs>
2023-05-15 12:47:42 +0200

    Added quotes

--- zsh/.config/zsh/config/04-path.zsh
+++ zsh/.config/zsh/config/04-path.zsh
@@ -1,3 +1,3 @@
-export PATH=/usr/local/sbin:$PATH
-export PATH=/usr/local/opt/ruby/bin:$PATH
-export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+export PATH="/usr/local/sbin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:$PATH"
