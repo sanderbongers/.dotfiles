ee2beb69e6cdfc0bf7ed7a03c0de364e301475d9
Sander Bongers <sander@bonge.rs>
2023-05-18 19:53:42 +0200

    Added function to interactively install brew packages

--- zsh/.config/zsh/config/00-functions.zsh
+++ zsh/.config/zsh/config/00-functions.zsh
@@ -1,7 +1,17 @@
-function command_exists() {
+command_exists() {
   if type "$1" &>/dev/null; then
     return 0 # command found
   else
     return 1 # command not found
+  fi
+}
+
+bip() {
+  local inst=$(brew search "$@" | fzf -m)
+
+  if [[ $inst ]]; then
+    for prog in $(echo $inst); do
+      brew install $prog
+    done
   fi
 }
