7a9d62124261d8b4c4f0f61e4f6634207d98242a
Sander Bongers <sander@bonge.rs>
2023-05-18 20:14:30 +0200

    Added interactive-cd

--- zsh/.config/zsh/config/00-plugins.zsh
+++ zsh/.config/zsh/config/00-plugins.zsh
@@ -1,16 +1,17 @@
 zsh_plugins=(
-  zsh-users/zsh-autosuggestions
-  zsh-users/zsh-syntax-highlighting
+  changyuheng/zsh-interactive-cd
   roosta/fif
   unixorn/tumult.plugin.zsh
   wfxr/forgit
+  zsh-users/zsh-autosuggestions
+  zsh-users/zsh-syntax-highlighting
 )
 
 # Download plugins
 for plugin in "${zsh_plugins[@]}"; do
   plugin_name=$(basename "$plugin")
   plugin_dir="$zsh_plugins_dir/$plugin_name"
-  [[ -d "$plugin_dir" ]] || git clone --quiet "git@github.com:$plugin.git" "$plugin_dir"
+  [[ -d "$plugin_dir" ]] || (echo "Installing $plugin_name..." && git clone --quiet "git@github.com:$plugin.git" "$plugin_dir")
 done
 
 # Load plugins
