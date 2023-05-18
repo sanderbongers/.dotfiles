d3b4741dd1935db1453dc42bc0531c0ef6c3ccfb
Sander Bongers <sander@bonge.rs>
2023-05-18 19:59:55 +0200

    Added comment

--- zsh/.config/zsh/config/00-functions.zsh
+++ zsh/.config/zsh/config/00-functions.zsh
@@ -1,17 +1,8 @@
+# Check if a command exists (i.e. is installed)
 command_exists() {
   if type "$1" &>/dev/null; then
     return 0 # command found
   else
     return 1 # command not found
-  fi
-}
-
-bip() {
-  local inst=$(brew search "$@" | fzf -m)
-
-  if [[ $inst ]]; then
-    for prog in $(echo $inst); do
-      brew install $prog
-    done
   fi
 }
