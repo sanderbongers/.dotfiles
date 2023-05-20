10601f052ff6d84787eb7dc28009260d9d4402de
Sander Bongers <sander@bonge.rs>
2023-05-20 03:32:13 +0200

    Added default fish config

--- zsh/.config/zsh/config/00-bindings.zsh
+++
@@ -1,6 +0,0 @@
-bindkey -e
-bindkey "^[b" backward-word    # ⌥←
-bindkey "^[f" forward-word     # ⌥→
-bindkey "^A" beginning-of-line # ⌘←
-bindkey "^E" end-of-line       # ⌘→
-bindkey "ç" fzf-cd-widget # ⌥c
