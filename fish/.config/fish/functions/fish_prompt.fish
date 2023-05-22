1a53afe697a759859b74790d6956087c168d0cd0
Sander Bongers <sander@bonge.rs>
2023-05-22 15:10:10 +0200

    Fixed test

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt
     set -l host ""
     # Set hostname when connected via ssh
-    test -z $SSH_CONNECTION; and set host (set_color $fish_color_host)$USER@$hostname(set_color normal)
+    test $SSH_CONNECTION; and set host (set_color $fish_color_host)$USER@$hostname(set_color normal)
 
     echo (echo $host (prompt_pwd) | string trim) '$ '
 end
