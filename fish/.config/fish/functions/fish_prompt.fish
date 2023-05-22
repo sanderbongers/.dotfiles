344b4d1ab20a31a78ee85416fcf72fa3856287da
Sander Bongers <sander@bonge.rs>
2023-05-22 13:04:54 +0200

    Fixed non-zero check

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    if test -z $SSH_CONNECTION
+    if test -n $SSH_CONNECTION
         set -l host $USER@$hostname
     end
     printf '%s' (set_color cyan) $host (set_color normal) (prompt_pwd) ' $ '
