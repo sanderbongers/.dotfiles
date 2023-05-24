cd88d052d6aa3eff6b8e099749de415faef9071b
Sander Bongers <sander@bonge.rs>
2023-05-24 21:15:37 +0200

    Changed host to always show unless explicitly set

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,9 @@
 function fish_prompt
     set -l host ""
-    # Set hostname when connected via ssh
-    test $SSH_CONNECTION; and set host (set_color $fish_color_host)$USER@$hostname(set_color normal)
+    if not set -q fish_prompt_show_host; or test "$fish_prompt_show_host" != "false";
+        set host (set_color $fish_color_host)$USER@$hostname(set_color normal)
+    end
+
 
     echo (echo $host (prompt_pwd) (fish_git_prompt) | tr -s " " | string trim) '$ '
 end
