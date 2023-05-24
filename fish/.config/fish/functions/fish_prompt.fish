bf6709236ccf576558f8da3acafef69abce6cac8
Sander Bongers <sander@bonge.rs>
2023-05-24 17:37:12 +0200

    Moved git variables to fish_variables

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,4 @@
 function fish_prompt
-    set -g __fish_git_prompt_show_informative_status true
-    set -g __fish_git_prompt_showcolorhints true
     set -l host ""
     # Set hostname when connected via ssh
     test $SSH_CONNECTION; and set host (set_color $fish_color_host)$USER@$hostname(set_color normal)
