9c359df7643cc4d4bed66b47ef94c5ed848190cc
Sander Bongers <sander@linku.nl>
2023-05-24 16:44:06 +0200

    Moved git prompt to the left prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,9 @@
 function fish_prompt
+    set -g __fish_git_prompt_show_informative_status true
+    set -g __fish_git_prompt_showcolorhints true
     set -l host ""
     # Set hostname when connected via ssh
     test $SSH_CONNECTION; and set host (set_color $fish_color_host)$USER@$hostname(set_color normal)
 
-    echo (echo $host (prompt_pwd) | string trim) '$ '
+    echo (echo $host (prompt_pwd) (fish_git_prompt) | tr -s " " | string trim) '$ '
 end
