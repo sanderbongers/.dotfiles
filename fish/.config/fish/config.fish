8b52f43dabd8c10f85067808a6c189715ce60b63
Sander Bongers <sander@bonge.rs>
2023-05-24 20:42:02 +0200

    Added config.local.fish for local configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,13 @@
+# Load local configuration
+set -l local_config $(status dirname)/config.local.fish
+test -f $local_config; and source $local_config
+
+# Add directories to $PATH
+set -l -a path_dirs $HOME/.local/bin $HOME/.config/fish/conf.d/bin $HOME/.cargo/bin /usr/local/opt/ruby/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/sbin /usr/local/opt/fzf/bin
+for dir in $path_dirs
+    test -d $dir; and set -gx PATH $dir $PATH
+end
+
 # Default editor
 if command -sq nvim
     set -gx EDITOR nvim
@@ -6,4 +16,4 @@ else
 end
 
 # Lazy load nvm
-test -d $HOME/.nvm && load_nvm >/dev/stderr
+test -d $HOME/.nvm; and load_nvm >/dev/stderr
