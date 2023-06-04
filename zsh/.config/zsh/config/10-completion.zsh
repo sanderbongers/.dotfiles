6df1a972c1ca94c9439e76067f09d7da38ac3d5c
Sander Bongers <sander@bonge.rs>
2023-06-04 17:24:24 +0200

    Remove function in favor of `zi`

--- zsh/.config/zsh/config/10-completion.zsh
+++ zsh/.config/zsh/config/10-completion.zsh
@@ -4,8 +4,3 @@ zmodload zsh/complist
 # Make Tab go straight to the menu and cycle there
 bindkey '\t' menu-select "$terminfo[kcbt]" menu-select
 bindkey -M menuselect '\t' menu-complete "$terminfo[kcbt]" reverse-menu-complete
-
-# Use zoxide as backend for cd completion
-+autocomplete:recent-directories() {
-    reply=(${(f)"$(zoxide query --list)"})
-}
