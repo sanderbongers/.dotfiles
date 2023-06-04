4f65820cf4a8a64a3550b08661b607aceeecc341
Sander Bongers <sander@bonge.rs>
2023-06-04 23:54:49 +0200

    Expanded the mkdir function
    It can handle multiple directories such as "one" "one/two" and "one/two thr{xx,yy}ee". But the optional flags can still break the command.

--- fish/.config/fish/functions/mkdir.fish
+++ fish/.config/fish/functions/mkdir.fish
@@ -1,11 +1,18 @@
-function mkdir --description "Create and move into a directory"
-    command mkdir -pv $argv
-    if test $status = 0
-        switch $argv[(count $argv)]
-            case '-*'
-            case '*'
-                cd $argv[(count $argv)]
-                return
-        end
+function mkdir --description 'Create and directly move into a directory'
+    set -l count (count $argv)
+    set -l noun directory
+
+    # Pluralize the noun when the argument contains a slash or when there are multiple arguments
+    if string match -q -r -- '\w/.+' $argv[1] || test $count -ne 1
+        set noun directories
     end
+
+    # Create the director(y/ies)
+    for d in $argv[1..-1]
+        mkdir -p $d
+        echo "Created $noun: $d"
+    end
+
+    # And move into the last one
+    cd $argv[-1]
 end
