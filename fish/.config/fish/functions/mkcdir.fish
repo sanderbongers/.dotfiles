693b4867fed90c36dd9093fbb6fb3f9e015068f5
Sander Bongers <sander@bonge.rs>
2023-06-05 18:55:40 +0200

    Continue traversing until a non-option so actual dir

---
+++ fish/.config/fish/functions/mkcdir.fish
@@ -0,0 +1,10 @@
+function mkcdir --wraps mkdir --description 'Create and directly move into a directory'
+    command mkdir -pv $argv
+    if test $status = 0
+        while string match -- '-*' $argv[-1]
+            set --erase argv[-1]
+        end
+        cd $argv[-1]
+        return
+    end
+end
