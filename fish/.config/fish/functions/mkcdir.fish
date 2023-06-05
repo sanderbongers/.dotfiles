0632dc50dd329ddcc40a60e34ccb68520da67e3d
Sander Bongers <sander@bonge.rs>
2023-06-05 19:54:56 +0200

    Change single quotes to double

--- fish/.config/fish/functions/mkcdir.fish
+++ fish/.config/fish/functions/mkcdir.fish
@@ -1,7 +1,7 @@
-function mkcdir --wraps mkdir --description 'Create and directly move into a directory'
+function mkcdir --wraps mkdir --description "Create and directly move into a directory"
     command mkdir -pv $argv
     if test $status = 0
-        while string match -- '-*' $argv[-1]
+        while string match -- "-*" $argv[-1]
             set --erase argv[-1]
         end
         cd $argv[-1]
