d1b3e42241ff7bf06c9b346cb418193ebb5af522
Sander Bongers <sander@bonge.rs>
2023-06-06 00:30:35 +0200

    Use descriptive options

--- fish/.config/fish/conf.d/bat.fish
+++ fish/.config/fish/conf.d/bat.fish
@@ -1,6 +1,4 @@
-# https://github.com/sharkdp/bat
-
 if command -sq bat
     set -gx BAT_CONFIG_PATH $HOME/.batrc
-    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+    set -gx MANPAGER "sh -c 'col -bx | bat --language=man --plain'"
 end
