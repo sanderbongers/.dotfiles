790e3ea1ef1cb02c8e9fe8aa07cf285387d4d24b
Sander Bongers <sander@bonge.rs>
2023-06-05 17:28:36 +0200

    Move aliases to gitconfig and create fish aliases

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -5,17 +5,12 @@ alias rm="rm -Iv"
 alias mkdir="mkdir -pv"
 
 # Git
-alias gcm="git commit -m"
-alias gco="git checkout"
-alias gcob="git checkout -b"
-alias gds="git diff --staged"
-alias gl="git log --graph --pretty=format:'%C(auto)%h%d %s %C(black)%C(bold)%cr %C(blue)%an%Creset' --color=always --abbrev-commit --date=relative"
-alias gls="git log --graph --pretty=format:'%C(auto)%h%d %s %C(black)%C(bold)%cr %C(blue)%an%Creset' --color=always --abbrev-commit --date=relative --stat"
-alias gp="git push"
-alias gpl="git pull"
-alias gs="git status -sb"
-alias gsh="git show"
-alias groot='cd (git rev-parse --show-toplevel)'
+alias groot="cd (git rev-parse --show-toplevel)"
+for git_alias in $(git config --get-regexp ^alias\. | string replace "alias." "g")
+    echo $git_alias | read -d " " -l alias -l command
+    alias $alias="git $command"
+    complete --command $alias --wraps git
+end
 
 # Other
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
