a7cafbb4d1ab2b28bf7d3cc05fce5907a468dd23
Sander Bongers <sander@bonge.rs>
2023-06-06 21:18:01 +0200

    Add fzf options

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,12 +1,15 @@
 # https://github.com/junegunn/fzf
 
-bind \u00E7 fzf-cd-widget
-
-set -gx FZF_DEFAULT_OPTS "--height 50% --reverse --border --multi --exact --cycle --preview-window hidden --bind 'ctrl-/:toggle-preview'"
+set -gx FZF_DEFAULT_OPTS "--height 50% --reverse --border --multi --exact --cycle --no-separator --preview-window hidden --bind 'ctrl-/:toggle-preview'"
+set -gx FZF_CTRL_R_OPTS "--select-1 --exit-0
+    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+    --header 'Press CTRL-Y to copy'"
+set -gx FZF_ALT_C_OPTS "--preview 'exa -1 --level=1 --color=never {}"
 
 # Use ripgrep to find files
 if command -sq rg
-    set -gx FZF_DEFAULT_COMMAND "rg --files 2>/dev/null"
+    set -gx FZF_DEFAULT_COMMAND "rg --files \$dir 2>/dev/null | sed '1d; s#^\./##'"
+    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 end
 
 # Use fd to find directories
@@ -17,5 +20,5 @@ end
 
 # Use bat to colorize previews
 if command -sq bat
-    set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'test -f {} && bat --color=always {}'"
+    set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'test -f {} && bat --color=always --line-range=:500 {}'"
 end
--- zsh/.config/zsh/config/fzf.zsh
+++ zsh/.config/zsh/config/fzf.zsh
@@ -1,22 +1,24 @@
 [[ -f "$HOME/.fzf.zsh" ]] && source "$HOME/.fzf.zsh"
 
-bindkey 'ç' fzf-history-widget
-
 export FZF_DEFAULT_OPTS="--height 50% --reverse --border --multi --exact --preview '' --preview-window hidden --bind 'ctrl-/:toggle-preview'"
-export FZF_CTRL_R_OPTS="--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
+export FZF_CTRL_R_OPTS="--select-1 --exit-0
+    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+    --color header:italic
+    --header 'ctrl+y to copy'"
+export FZF_ALT_C_OPTS="--select-1 --exit-0 --preview 'exa -1 --level=1 --color=never {} | head -100'"
 
 # Use ripgrep to find files
 if command_exists "rg"; then
-	export FZF_DEFAULT_COMMAND="rg --files 2> /dev/null"
-	export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+    export FZF_DEFAULT_COMMAND="rg --files 2> /dev/null"
+    export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 fi
 
 # Use fd to find directories
 if command_exists "fd"; then
-	export FZF_ALT_C_COMMAND="fd --type directory"
+    export FZF_ALT_C_COMMAND="fd --type directory"
 fi
 
 # Use bat to colorize previews
 if command_exists "bat"; then
-	export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --preview 'test -f {} && bat --color=always --line-range=:100 {}'"
+    export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --preview 'test -f {} && bat --color=always {}'"
 fi
