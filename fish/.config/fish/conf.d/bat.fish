a27026c9921f8a0f05fdc6e44514cff5d306d3aa
Sander Bongers <sander@bonge.rs>
2023-06-09 22:22:56 +0200

    Sort lines

--- fish/.config/fish/conf.d/bat.fish
+++ fish/.config/fish/conf.d/bat.fish
@@ -1,5 +1,5 @@
 if command -sq bat
     set -gx BAT_CONFIG_PATH $HOME/.batrc
-    set -gx MANPAGER "sh -c 'col -bx | bat --language=man --plain'"
     set -gx BAT_PAGER less -RF --mouse
+    set -gx MANPAGER "sh -c 'col -bx | bat --language=man --plain'"
 end
