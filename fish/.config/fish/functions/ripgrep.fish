d97694b29b864974cc3cd222adb3dd5a12c719dc
Sander Bongers <sander@bonge.rs>
2023-06-10 14:54:54 +0200

    Update function to use fzf as interactive ripgrep launcher

--- fish/.config/fish/functions/ripgrep.fish
+++ fish/.config/fish/functions/ripgrep.fish
@@ -1,21 +1,17 @@
 function ripgrep
-    set -f rg_prefix "rg --column --line-number --no-heading --color=always --smart-case"
+    set -f rg_prefix "rg --column --line-number --no-heading --color=always --smart-case --hidden"
 
-    rm -f /tmp/rg-fzf-{r,f}
-
-    : | FZF_DEFAULT_OPTS="" fzf
-    --ansi
-    --disabled
-    --query $argv
-    --bind "start:reload:$rg_prefix {q}"
-    --bind "change:reload:sleep 0.1; $rg_prefix {q} || true"
-    --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f)"
-    --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. ripgrep> )+disable-search+reload($rg_prefix {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r)"
-    --color "hl:-1:underline,hl+:-1:underline:reverse"
-    --prompt "1. ripgrep> "
-    --delimiter :
-    --header "╱ CTRL-R (ripgrep mode) ╱ CTRL-F (fzf mode) ╱"
-    --preview "bat --style=default --color=always {1} --highlight-line {2}"
-    --preview-window "up,60%,border-bottom,+{2}+3/3,~3"
-    --bind "enter:become(vim {1} +{2})"
+    : | FZF_DEFAULT_OPTS="" fzf \
+        --ansi \
+        --disabled \
+        --query "$argv" \
+        --height 100% \
+        --reverse \
+        --border none \
+        --bind "start:reload:$rg_prefix {q}" \
+        --bind "change:reload:sleep 0.05; $rg_prefix {q} || true" \
+        --delimiter : \
+        --preview 'bat --style=default --color=always {1} --highlight-line {2}' \
+        --preview-window 'down,60%,border-top,+{2}+3/3,~3' \
+        --bind 'enter:become(vim {1} +{2})'
 end
