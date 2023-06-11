3c66b9cf9e1725c654e0e0dd892f8eede870a246
Sander Bongers <sander@bonge.rs>
2023-06-11 04:43:55 +0200

    Add ripgrep-fzf toggle

--- fish/.config/fish/functions/ripgrep.fish
+++ fish/.config/fish/functions/ripgrep.fish
@@ -1,17 +1,23 @@
 function ripgrep
-    set -f rg_prefix "rg --column --line-number --no-heading --color=always --smart-case --hidden"
+    set -f rg_prefix "rg --no-config --no-heading --line-number --no-column --color=always --hidden --smart-case"
+
+    command rm -f /tmp/rg-fzf-{r,f}
 
     : | FZF_DEFAULT_OPTS="" fzf \
         --ansi \
         --disabled \
-        --query "$argv" \
+        --query $argv \
         --height 100% \
         --reverse \
         --border none \
-        --bind "start:reload:$rg_prefix {q}" \
+        --header "Toggle mode: ⌃r (ripgrep) | ⌃f (fzf)" \
+        --bind "start:reload($rg_prefix {q})+unbind(ctrl-r)" \
         --bind "change:reload:sleep 0.05; $rg_prefix {q} || true" \
+        --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(filter> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rg-fzf-r; command cat /tmp/rg-fzf-f)" \
+        --bind "ctrl-r:unbind(ctrl-r)+change-prompt(ripgrep> )+disable-search+reload($rg_prefix {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rg-fzf-f; command cat /tmp/rg-fzf-r)" \
+        --prompt "ripgrep> " \
         --delimiter : \
-        --preview 'bat --style=default --color=always {1} --highlight-line {2}' \
-        --preview-window 'down,60%,border-top,+{2}+3/3,~3' \
-        --bind 'enter:become(vim {1} +{2})'
+        --preview "bat --style=default --color=always {1} --highlight-line {2}" \
+        --preview-window "down,60%,border-top,~3,+{2}+3/3" \
+        --bind "enter:become(vim {1} +{2})"
 end
