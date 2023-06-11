112c282519ce3a8e490386c0dffc91b4a0ad458d
Sander Bongers <sander@bonge.rs>
2023-06-11 04:52:30 +0200

    Add underline to fzf match

--- fish/.config/fish/functions/ripgrep.fish
+++ fish/.config/fish/functions/ripgrep.fish
@@ -13,11 +13,12 @@ function ripgrep
         --header "Toggle mode: ⌃r (ripgrep) | ⌃f (fzf)" \
         --bind "start:reload($rg_prefix {q})+unbind(ctrl-r)" \
         --bind "change:reload:sleep 0.05; $rg_prefix {q} || true" \
-        --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(filter> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rg-fzf-r; command cat /tmp/rg-fzf-f)" \
+        --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rg-fzf-r; command cat /tmp/rg-fzf-f)" \
         --bind "ctrl-r:unbind(ctrl-r)+change-prompt(ripgrep> )+disable-search+reload($rg_prefix {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rg-fzf-f; command cat /tmp/rg-fzf-r)" \
         --prompt "ripgrep> " \
         --delimiter : \
         --preview "bat --style=default --color=always {1} --highlight-line {2}" \
         --preview-window "down,60%,border-top,~3,+{2}+3/3" \
+        --color "hl:-1:underline,hl+:-1:underline:reverse" \
         --bind "enter:become(vim {1} +{2})"
 end
