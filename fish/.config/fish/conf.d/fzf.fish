89e02ed409dc59ec5805e82f3d21f6a1686432df
Sander Bongers <sander@bonge.rs>
2023-06-12 04:21:48 +0200

    Add file preview to default opts, use fzf_cd_toggle in alt-c command

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,22 +1,22 @@
 # https://github.com/junegunn/fzf
 
-set -gx FZF_DEFAULT_OPTS "--height 100% --reverse --multi --exact --bind 'ctrl-/:toggle-preview' --preview-window 'hidden,down,60%'"
-set -gx FZF_CTRL_R_OPTS "--header 'Copy: ⌃y' --bind 'ctrl-y:execute-silent(echo -n {} | pbcopy)+abort' --bind 'ctrl-/:ignore'"
-set -gx FZF_ALT_C_OPTS "--preview 'exa -1 --level=1 --color=never {}'"
+set -gx FZF_DEFAULT_OPTS "--height 100% --reverse --multi --exact --preview-window 'hidden,down,60%' --bind 'ctrl-/:toggle-preview+transform-preview-label(echo [ {} ])' --bind 'ctrl-z:ignore'"
+set -gx FZF_CTRL_R_OPTS "--header 'Copy: ⌃y' --bind 'ctrl-y:execute-silent(echo -n {} | pbcopy)+abort' --bind 'ctrl-r:ignore,ctrl-/:ignore'"
 
 # Use ripgrep to find files
 if command -sq rg
-    set -gx FZF_DEFAULT_COMMAND "command rg --ignore-file=$HOME/.rgignore --files \$dir 2>/dev/null | sed '1d; s#^\./##'"
+    set -gx FZF_DEFAULT_COMMAND "command rg --ignore-file=$HOME/.rgignore --files \$dir 2>/dev/null | sed 's@^\./@@'"
     set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 end
 
 # Use fd to find directories
 set -l fd (command -s fd || command -s fdfind)
 if command -sq $fd
-    set -gx FZF_ALT_C_COMMAND "$fd --ignore-file=$HOME/.rgignore --type directory --hidden"
+    set -gx FZF_ALT_C_COMMAND "$fd --ignore-file=$HOME/.rgignore --type directory"
+    set -gx FZF_ALT_C_OPTS "--header 'Toggle hidden: ⌃h' --bind 'start:execute-silent(fzf_cd_toggle init)' --bind 'ctrl-h:transform-query(fzf_cd_toggle pre {q})+reload(eval {q})+transform-query(fzf_cd_toggle post)'"
 end
 
 # Use bat to colorize previews
 if command -sq bat
-    set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'test -f {} && bat --color always --line-range :500 {}'"
+    set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'test -f {} && bat --color always --line-range :500 {} || exa -1 --level=1 --color=never {}' --bind 'focus:transform-preview-label(echo [ {} ])'"
 end
