85bc6146d941cfa7f37af639977cd72fdf3eecd6
Sander Bongers <sander@bonge.rs>
2023-06-18 00:10:55 +0200

    Use --all twice to also show . and .. directories

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,7 +1,7 @@
 function ll --wraps=exa
     # If installed, use exa as ls replacement
     if command -sq exa
-        exa --long --all --binary --classify --group --time-style=iso $argv
+        exa --long --all --all --binary --classify --group --time-style=iso $argv
         return
     end
 
