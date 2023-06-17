a2f2edce9599eb7c51edbe8dee40a2fbcccdf9cf
Sander Bongers <sander@bonge.rs>
2023-06-17 21:56:18 +0200

    Move .batrc to the default .config/bat/config

--- fish/.config/fish/conf.d/bat.fish
+++ fish/.config/fish/conf.d/bat.fish
@@ -1,7 +1,5 @@
 # https://github.com/sharkdp/bat
 
 if command -sq bat
-    set -gx BAT_CONFIG_PATH $HOME/.batrc
-    set -gx BAT_PAGER less -RF --mouse
     set -gx MANPAGER "sh -c 'col -bx | bat --language=man --plain'"
 end
