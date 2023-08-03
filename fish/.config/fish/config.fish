cbe426b419ae8abe3f050b40cc3551843ea0bc0c
Sander Bongers <sander@bonge.rs>
2023-08-03 22:42:29 +0200

    Remove gnutils

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,7 @@ set -l local_config $__fish_config_dir/config.local.fish
 test -f $local_config; and source $local_config
 
 # Add directories to $PATH
-set -l -a path_dirs $__fish_config_dir/conf.d/bin $HOME/.local/bin $HOME/.cargo/bin /usr/local/opt/ruby/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/sbin /usr/local/opt/fzf/bin
+set -l -a path_dirs $__fish_config_dir/conf.d/bin $HOME/.local/bin $HOME/.cargo/bin /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/opt/fzf/bin
 for dir in $path_dirs
     test -d $dir; and set -gx PATH $dir $PATH
 end
