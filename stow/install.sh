c3b7104bae421160a7ad9f47899a6eb7a6cb4e5c
Sander Bongers <sander@bonge.rs>
2023-08-03 23:26:46 +0200

    Apply shellcheck suggestions

--- stow/install.sh
+++ stow/install.sh
@@ -5,16 +5,16 @@ current_dir="$(dirname "$0")"
 # Setup and install OS-specific packages
 case "$(uname -s)" in
 Linux)
-	source "$current_dir/install-linux.sh"
+	. "$current_dir/install-linux.sh"
 	;;
 Darwin)
-	source "$current_dir/install-macos.sh"
+	. "$current_dir/install-macos.sh"
 	;;
 esac
 
 # Set fish as default shell
 shell_path="$(command -v fish)"
-if [[ $shell_path && $shell_path != "$SHELL" ]]; then
+if [ "$shell_path" ] && [ "$shell_path" != "$SHELL" ]; then
 	# Add to /etc/shells if not present
 	if ! grep -Fxq "$shell_path" /etc/shells; then
 		echo "$shell_path" | sudo tee -a /etc/shells
