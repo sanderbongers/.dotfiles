2a2c5cebfdf251347ecea78a2e8c3259ae07d751
Sander Bongers <sander@bonge.rs>
2023-11-06 20:07:14 +0100

    Clarify comment and command

--- stow/install.sh
+++ stow/install.sh
@@ -15,11 +15,11 @@ esac
 # Set fish as default shell
 shell_path="$(command -v fish)"
 if [ "$shell_path" ] && [ "$shell_path" != "$SHELL" ]; then
-	# Add to /etc/shells if not present
+	# Add fish to /etc/shells if not present
 	if ! grep -Fxq "$shell_path" /etc/shells; then
 		echo "$shell_path" | sudo tee -a /etc/shells
 	fi
 
 	# Set as default shell
-	chsh -s "$shell_path"
+	chsh --shell "$shell_path"
 fi
