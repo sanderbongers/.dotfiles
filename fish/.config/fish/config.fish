983a6601c342f66bd73a77c59d829c90cb64fd47
Sander Bongers <sander@bonge.rs>
2023-11-06 17:30:05 +0100

    Add ssh identities to ssh-agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,19 @@
-# Load local configuration
+# Load local Fish configuration
 set -l local_config $__fish_config_dir/config.local.fish
 test -f $local_config; and source $local_config
 
-# Add directories to $PATH
+# Add binary directories to $PATH
 set -l -a path_dirs $__fish_config_dir/conf.d/bin $HOME/.local/bin $HOME/.cargo/bin /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/opt/fzf/bin
 for dir in $path_dirs
     test -d $dir; and set -gx PATH $dir $PATH
+end
+
+# Add identities to ssh-agent
+switch (uname -s)
+    case Linux
+        keychain --eval --quiet --quick $HOME/.ssh/id_ed25519 | source
+    case Darwin
+        /usr/bin/ssh-add --apple-load-keychain -q
 end
 
 # Default editor
