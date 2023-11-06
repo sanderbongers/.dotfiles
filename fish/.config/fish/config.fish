36fb631dbccf5c3bbe502a1a97beb5f343e2f869
Sander Bongers <sander@bonge.rs>
2023-11-06 17:42:31 +0100

    Add check if keychain is installed

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,7 +11,7 @@ end
 # Add identities to ssh-agent
 switch (uname -s)
     case Linux
-        keychain --eval --quiet --quick $HOME/.ssh/id_ed25519 | source
+        which keychain; and keychain --eval --quiet --quick $HOME/.ssh/id_ed25519 | source
     case Darwin
         /usr/bin/ssh-add --apple-load-keychain -q
 end
