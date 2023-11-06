8a3c188a7cc5b5d9233942c0fde51ee1b1648cf7
Sander Bongers <sander@bonge.rs>
2023-11-06 18:17:09 +0100

    Move z aliases from fish_variables to zoxide.fish

--- fish/.config/fish/conf.d/zoxide.fish
+++ fish/.config/fish/conf.d/zoxide.fish
@@ -1,3 +1,9 @@
 if command -sq zoxide
-  zoxide init fish | source
+    zoxide init fish | source
+
+    # Additional aliases
+    abbr -a -- zr 'zoxide remove'
+    abbr -a -- zqi 'zoxide query -i'
+    abbr -a -- zq 'zoxide query'
+    abbr -a -- za 'zoxide add'
 end
