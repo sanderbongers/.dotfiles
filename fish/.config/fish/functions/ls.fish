74d1ab3c967b530ea323461fc91ba4bef8bfe610
Sander Bongers <sander@bonge.rs>
2024-01-12 17:12:21 +0100

    Show all files and group owner by default

--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -1,9 +1,9 @@
 function ls --wraps=exa
     # If installed, use exa as ls replacement
     if command -sq exa
-        exa $argv
+        exa --all --group $argv
         return
     end
 
-    command ls --color=auto $argv
+    command ls -a --color=auto $argv
 end
