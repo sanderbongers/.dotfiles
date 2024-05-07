9b5af30f4d735daa312a0ffe39196df6aa62ac0a
Sander Bongers <sander@bonge.rs>
2024-05-07 14:02:11 +0200

    Do not output a new line after the prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -4,5 +4,5 @@ function fish_prompt
         set host (set_color $fish_color_host)$USER@$hostname(set_color normal)
     end
 
-    echo (echo $host (prompt_pwd) (fish_git_prompt) | tr -s " " | string trim) '$ '
+    echo -n (echo $host (prompt_pwd) (fish_git_prompt) | tr -s " " | string trim) '$ '
 end
