acbb73acc9a9ac8b7e490d4ec54f3c6208d2c53c
Sander Bongers <sander@bonge.rs>
2024-05-08 16:28:03 +0200

    Remove double --all

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,7 +1,7 @@
 function ll --wraps=exa
     # If installed, use exa as ls replacement
     if command -sq exa
-        exa --long --all --all --classify --group --time-style=iso $argv
+        exa --long --classify --group --time-style=iso $argv
         return
     end
 
