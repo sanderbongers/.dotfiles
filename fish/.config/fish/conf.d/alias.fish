2fa3d8d3bb54ae1f1711ac07fb9ebe281dbb3192
Sander Bongers <sander@bonge.rs>
2024-05-09 01:30:57 +0200

    Disable Forgit aliases

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -4,10 +4,10 @@ alias rm="rm -Iv"
 alias mkdir="mkdir -pv"
 alias mv="mv -iv"
 
-# Git
+# Turn gitconfig aliases into Fish aliases
 alias groot="cd (git rev-parse --show-toplevel)"
 for git_alias in (git config --get-regexp "^alias\." | string replace "alias." "g")
-    echo $git_alias | read -d " " -l alias -l command
+    echo $git_alias | read --delimiter " " --local alias --local command
     alias $alias="git $command"
     complete --command $alias --wraps git
 end
@@ -17,3 +17,6 @@ alias myip="curl -s http://httpbin.org/ip | jq --raw-output '.[]'"
 
 # Abbrevations
 abbr --add bs "brew services"
+
+# Disable Forgit aliases
+set -x FORGIT_NO_ALIASES true
