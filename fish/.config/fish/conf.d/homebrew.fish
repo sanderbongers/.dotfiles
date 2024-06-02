8c85126629f04557bab59b14f9b0bae090228961
Sander Bongers <sander@bonge.rs>
2024-06-02 10:49:22 +0200

    Changed comment

--- fish/.config/fish/conf.d/homebrew.fish
+++ fish/.config/fish/conf.d/homebrew.fish
@@ -1,7 +1,7 @@
 set -gx HOMEBREW_BUNDLE_FILE $HOME/.config/homebrew/Brewfile
 set -gx HOMEBREW_NO_ENV_HINTS 1
 
-# Use Homebrew's Ruby to install gems
+# Use Homebrew's Ruby gems to install gems
 if command -q brew-gem
     abbr --add gem "brew gem"
 end
