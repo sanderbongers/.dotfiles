58f303d9803bdae98e97ec000efa4097405fa14b
Sander Bongers <sander@bonge.rs>
2024-08-31 21:21:11 +0200

    Update macOS defaults

--- stow/install-macos.sh
+++ stow/install-macos.sh
@@ -32,32 +32,41 @@ ssh_key_path="$HOME/.ssh/id_ed25519"
 test -f "$ssh_key_path" && /usr/bin/ssh-add --apple-use-keychain "$ssh_key_path"
 
 # Set macOS user defaults
-# chflags nohidden ~/Library                                                           # Show the ~/Library folder
-# defaults write -globalDomain AppleKeyboardUIMode -int 3                              # Enable full keyboard access for all controls
-# defaults write -globalDomain NSAutomaticQuoteSubstitutionEnabled -bool false         # Disable smart quotes
-# defaults write -globalDomain NSNavPanelExpandedStateForSaveMode -bool true           # Expand save panel by default
-# defaults write -globalDomain PMPrintingExpandedStateForPrint2 -bool true             # Expand print panel by default
-# defaults write com.apple.appleseed.FeedbackAssistant Autogather -bool false          # Do not autogather large files when submitting feedback
-# defaults write com.apple.dock magnification -bool false                              # Disable Dock magnification
-# defaults write com.apple.dock show-recents -bool false                               # Disable recent apps in Dock
-# defaults write com.apple.dock autohide -bool true                                    # Auto-hide Dock
-# defaults write com.apple.dock tilesize -int 48                                       # Set Dock icon size
-# defaults write com.apple.finder _FXSortFoldersFirst -bool true                       # Keep folders on top when sorting by name
-# defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false           # Disable warning when changing file extension
-# defaults write com.apple.finder NewWindowTargetPath "file://$HOME/Downloads"         # Set default Finder location
-# defaults write com.apple.finder QuitMenuItem -bool true                              # Show Quit Finder menu item
-# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false          # Hide external disks on desktop
-# defaults write com.apple.finder ShowPathbar -bool true                               # Show Finder path bar
-# defaults write com.apple.helpviewer HVIncludesKBSearches -bool false                 # Remove Apple KB articles from Help menu
-# defaults write com.apple.LaunchServices LSQuarantine -bool false                     # Disable "Are you sure you want to open this application?" dialog
-# defaults write com.apple.Safari HomePage -string "about:blank"                       # Set Safari homepage to about:blank
-# defaults write com.apple.Safari IncludeDebugMenu -boolean true                       # Enable Safari debug menu
-# defaults write com.apple.safari ShowFullURLInSmartSearchField -bool true             # Show full URL in Safari
-# defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true # Enable Safari developer tools
-# defaults write com.apple.screencapture disable-shadow -bool true                     # Disable screenshot shadow
-defaults write com.apple.screencapture include-date -bool false # Exclude screenshot date
-# defaults write com.apple.screencapture location ~/Downloads                          # Set screenshot location
-# defaults write com.apple.screencapture show-thumbnail -bool false                    # Disable screenshot thumbnail
-# defaults write com.apple.TextEdit RichText -bool false                               # Use plain text mode for new TextEdit documents
-# defaults write com.apple.universalaccess showWindowTitlebarIcons -bool true          # Show icons in window title bars
-# defaults write NSGlobalDomain AppleShowAllExtensions -bool true                      # Show file extensions
+defaults write com.apple.appleseed.FeedbackAssistant Autogather -bool false
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0.2
+defaults write com.apple.dock autohide-time-modifier -float 0.33
+defaults write com.apple.dock magnification -bool false
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock size-immutable -bool true
+defaults write com.apple.dock tilesize -int 48
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder NewWindowTargetPath "file://$HOME/Downloads"
+defaults write com.apple.finder QuitMenuItem -bool true
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+defaults write com.apple.helpviewer HVIncludesKBSearches -bool false
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+defaults write com.apple.Safari HomePage -string "about:blank"
+defaults write com.apple.Safari IncludeDebugMenu -boolean true
+defaults write com.apple.Safari IncludeDevelopMenu -boolean true
+defaults write com.apple.safari ShowFullURLInSmartSearchField -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.screencapture disable-shadow -bool true
+defaults write com.apple.screencapture include-date -bool false
+defaults write com.apple.screencapture location "$HOME/Downloads"
+defaults write com.apple.screencapture show-thumbnail -bool false
+defaults write com.apple.TextEdit RichText -bool false
+defaults write com.apple.universalaccess showWindowTitlebarIcons -bool true
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+defautls write com.apple.finder FXRemoveOldTrashItems -bool true
+killall Dock
+killall Finder
+chflags nohidden ~/Library
