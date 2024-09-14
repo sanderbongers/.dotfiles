f9a04626a81dbc89195c366e7fc9afa6f3dd2351
Sander Bongers <sander@bonge.rs>
2024-09-14 10:04:22 +0200

    Remove ssh-add because ssh config already has AddKeysToAgent

--- fish/.config/fish/conf.d/ssh.fish
+++ fish/.config/fish/conf.d/ssh.fish
@@ -2,6 +2,4 @@
 switch (uname -s)
     case Linux
         command -q keychain; and keychain --eval --quiet --quick $HOME/.ssh/id_ed25519 | source
-    case Darwin
-        /usr/bin/ssh-add --apple-load-keychain -q
 end
