a1bfb594ffc1820d7c6afb2e439d23d6b3e9a156
Sander Bongers <sander@bonge.rs>
2024-09-14 07:52:38 +0200

    Install Monaco Nerd font if applicable

---
+++ fish/.config/fish/conf.d/fonts.fish
@@ -0,0 +1,6 @@
+# Download and install the Monaco Nerd font if it's not already installed
+if not test -e ~/Library/Fonts/MonacoNerdFont-Regular.ttf
+    curl -fLO https://github.com/thep0y/monaco-nerd-font/releases/download/v0.2.1/MonacoNerdFont.zip
+    unzip -o MonacoNerdFont.zip -d ~/Library/Fonts
+    rm MonacoNerdFont.zip
+end
