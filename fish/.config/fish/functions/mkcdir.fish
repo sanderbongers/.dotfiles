6bf39ca2265cef304b32b9e0d13ef00f5b901445
Sander Bongers <sander@bonge.rs>
2024-09-15 12:57:10 +0200

    Update function with code from official docs

--- fish/.config/fish/functions/mkcdir.fish
+++ fish/.config/fish/functions/mkcdir.fish
@@ -1,10 +1,12 @@
 function mkcdir --wraps mkdir --description "Create a directory and directly move into it"
     command mkdir -pv $argv
     if test $status = 0
-        while string match -- "-*" $argv[-1]
-            set --erase argv[-1]
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
         end
-        cd $argv[-1]
-        return
     end
 end
