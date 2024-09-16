c0aa8d247344b41335b66a1e2a30613fd29e020d
Sander Bongers <sander@bonge.rs>
2024-09-16 06:08:52 +0200

    Set variables

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,25 +1,27 @@
 require("config.lazy")
 
 local vim = vim
+local a = vim.api
+local o = vim.opt
 
 -- Synchronize with system clipboard
-vim.opt.clipboard:append("unnamed,unnamedplus")
+o.clipboard:append("unnamed,unnamedplus")
 
 -- Block style cursor in all modes
-vim.opt.guicursor = ""
+o.guicursor = ""
 
 -- Case insensitive search
-vim.opt.ignorecase = true
+o.ignorecase = true
 
 -- Autoindent when starting a new line
-vim.opt.smartindent = true
+o.smartindent = true
 
 -- Complete first match and list all matches
-vim.opt.wildmode = "list:full"
+o.wildmode = "list:full"
 
 -- Enable mouse scrolling in iTerm2
-vim.api.nvim_set_keymap('n', '<ScrollWheelDown>', 'j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<ScrollWheelUp>', 'k', { noremap = true, silent = true })
+a.nvim_set_keymap('n', '<ScrollWheelDown>', 'j', { noremap = true, silent = true })
+a.nvim_set_keymap('n', '<ScrollWheelUp>', 'k', { noremap = true, silent = true })
 
 -- Enable fzf in Vim
-vim.opt.rtp:append("/usr/local/opt/fzf")
+o.rtp:append("/usr/local/opt/fzf")
