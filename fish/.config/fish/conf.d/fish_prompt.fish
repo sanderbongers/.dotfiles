a19cd2e569b6826a383c68711865b14134ecd35a
Sander Bongers <sander@bonge.rs>
2024-09-16 05:57:43 +0200

    Reorganised variables

--- fish/.config/fish/conf.d/fish_prompt.fish
+++ fish/.config/fish/conf.d/fish_prompt.fish
@@ -3,20 +3,28 @@ function fish_prompt
     set -q fish_color_host_remote; or set -U fish_color_host_remote cyan
     set -q fish_color_status; or set -U fish_color_status red
 
-    set -l prompt_login ""
     if not set -q fish_prompt_show_host; or test "$fish_prompt_show_host" != false
-        set prompt_login (printf '%s%s@%s%s ' (set_color $fish_color_host_remote) (whoami) (prompt_hostname) (set_color normal))
+        printf "%s" (set_color $fish_color_host_remote)
+        printf "%s@%s" (whoami) (prompt_hostname)
+        printf "%s " (set_color normal)
     end
 
-    echo -n -s $prompt_login (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (fish_git_prompt) (set_color normal) ' $ '
+    printf "%s" (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    printf "%s" (fish_git_prompt)
+    printf ' $ '
 end
 
 function fish_right_prompt
     set -l last_pipestatus $pipestatus
-    set -lx __fish_last_status $status # Export for __fish_print_pipestatus
+    set -lx __fish_last_status $status
 
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color --bold $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "✘ " "" "|" "$status_color" "$statusb_color" $last_pipestatus)
-    echo -n -s $prompt_status (set_color normal)
+    set -l left_brace "✘ "
+    set -l right_brace ""
+    set -l separator " "
+    set -l brace_sep_color (set_color $fish_color_status)
+    set -l status_color (set_color --bold $fish_color_status)
+
+    set -l prompt_status (__fish_print_pipestatus $left_brace $right_brace $separator $brace_sep_color $status_color $last_pipestatus)
+
+    printf "%s" $prompt_status
 end
