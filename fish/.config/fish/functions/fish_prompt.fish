ede49b461d9eb1be8739b045aa891ca95751ee64
Sander Bongers <sander@bonge.rs>
2024-10-05 01:02:09 +0200

    Fix spacing

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -13,7 +13,7 @@ function fish_prompt
 
     if not set -q fish_prompt_show_host; or not contains -- $fish_prompt_show_host no false 0
         set_color $fish_color_host_remote
-        printf "%s@%s" (whoami) (prompt_hostname)
+        printf "%s@%s " (whoami) (prompt_hostname)
         set_color normal
     end
 
