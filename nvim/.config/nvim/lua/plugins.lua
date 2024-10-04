478e0f876e2bd9f972b69215c8c15f1973ae47e0
Sander Bongers <sander@bonge.rs>
2024-10-04 20:26:59 +0200

    Enable Copilot on Insert mode

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -13,6 +13,7 @@ return {
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
+        event = "InsertEnter",
         opts = {
             suggestion = {
                 auto_trigger = true
