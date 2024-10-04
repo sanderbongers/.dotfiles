d03c0c7018e57926766bc7b78e534f65e0a58caf
Sander Bongers <sander@bonge.rs>
2024-10-05 01:46:06 +0200

    Install eza

--- var/install-linux.sh
+++ var/install-linux.sh
@@ -5,7 +5,6 @@ if [[ "$1" != "linux" ]]; then
 	exit 1
 fi
 
-sudo add-apt-repository ppa:git-core/ppa
 sudo apt update -y
 sudo apt install -y \
 	bat \
@@ -18,6 +17,14 @@ sudo apt install -y \
 	ripgrep \
 	stow \
 	zoxide
+
+# Install eza
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 # shellcheck disable=SC2035
 stow --target "$HOME" */
