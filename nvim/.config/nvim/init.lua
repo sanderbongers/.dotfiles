9a11b93ea9a029edfd9adadbcb83d273690784b9
Sander Bongers <sander@bonge.rs>
2024-10-04 19:43:47 +0200

    Set more default nvim optiosn

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,14 +7,28 @@ local o = vim.opt
 -- Synchronize with system clipboard
 o.clipboard:append("unnamed,unnamedplus")
 
+-- Highlight current line
+o.cursorline = true
+
 -- Block style cursor in all modes
 o.guicursor = ""
 
 -- Case insensitive search
 o.ignorecase = true
 
+-- Print line numbers, relative to the current line
+o.number = true
+o.relativenumber = true
+
 -- Autoindent when starting a new line
 o.smartindent = true
+
+-- Splitting a window will put the new window below or right of the current one
+o.splitbelow = true
+o.splitright = true
+
+-- Show filename in window title
+o.title = true
 
 -- Complete first match and list all matches
 o.wildmode = "list:full"
