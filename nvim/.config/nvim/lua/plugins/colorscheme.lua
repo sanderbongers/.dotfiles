1d51e1ba15019f910561dd73893f81e08e5444af
Sander Bongers <sander@bonge.rs>
2024-10-06 05:55:33 +0200

    Adjusted colors a bit

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,22 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require("tokyonight").setup({ transparent = true })
+        require("tokyonight").setup({
+            style = "night",
+            on_colors = function(c)
+                c.bg = "#19191d"
+                c.bg_dark = "#1e2030"
+                c.bg_float = "#1e2030"
+                c.bg_popup = "#1e2030"
+                c.bg_sidebar = "#1e2030"
+                c.bg_statusline = "#1e2030"
+            end,
+            on_highlights = function(hl, c)
+                hl.CursorLine = {
+                    bg = "#21232d"
+                }
+            end
+        })
         vim.cmd("colorscheme tokyonight")
-    end,
+    end
 }
