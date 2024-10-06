f1401f492165f326787cebe1481a54cccf89657b
Sander Bongers <sander@bonge.rs>
2024-10-06 03:51:36 +0200

    Add tokyonight theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,9 +2,8 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { transparent = true },
-    config = function(_, opts)
-        require("tokyonight").setup(opts)
-        vim.cmd([[colorscheme tokyonight]])
+    config = function()
+        require("tokyonight").setup({ transparent = true })
+        vim.cmd("colorscheme tokyonight")
     end,
 }
