deefa8d5b4037aa8b631f68d45734f4f799fc918
Sander Bongers <sander@bonge.rs>
2024-10-13 03:14:49 +0200

    Configured LSP

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,11 +1,15 @@
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-emoji",
         "onsails/lspkind.nvim",
-        "saadparwaiz1/cmp_luasnip",
         {
             "zbirenbaum/copilot-cmp",
             dependencies = "copilot.lua",
@@ -14,8 +18,58 @@ return {
             end,
         },
     },
-    opts = function(_, opts)
-        local luasnip = require("luasnip")
+    config = function()
+        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+        vim.opt.signcolumn = "yes"
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "✘",
+                    [vim.diagnostic.severity.WARN] = "▲",
+                    [vim.diagnostic.severity.HINT] = "⚑",
+                    [vim.diagnostic.severity.INFO] = "»",
+                },
+            },
+        })
+
+        local lspconfig = require("lspconfig")
+        lspconfig.util.default_config.capabilities = vim.tbl_deep_extend(
+            "force",
+            lspconfig.util.default_config.capabilities,
+            require("cmp_nvim_lsp").default_capabilities()
+        )
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            desc = "LSP actions",
+            callback = function(event)
+                local opts = { buffer = event.buf }
+
+                vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+                vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+                vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+                vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+                vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+                vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+                vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+                vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+                vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+                vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+            end,
+        })
+
+        require("mason").setup({})
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "intelephense",
+                "lua_ls",
+            },
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+            },
+        })
+
         local cmp = require("cmp")
         cmp.setup {
             formatting = {
@@ -24,24 +78,13 @@ return {
                 }),
             },
             mapping = {
-                ["<CR>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() and cmp.get_active_entry() then
-                        if luasnip.expandable() then
-                            luasnip.expand()
-                        else
-                            cmp.confirm({
-                                select = true,
-                            })
-                        end
-                    else
-                        fallback()
-                    end
-                end),
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = false,
+                }),
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
-                    elseif luasnip.locally_jumpable(1) then
-                        luasnip.jump(1)
                     else
                         fallback()
                     end
@@ -49,8 +92,6 @@ return {
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
                     else
                         fallback()
                     end
@@ -58,14 +99,15 @@ return {
             },
             snippet = {
                 expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
+                    vim.snippet.expand(args.body)
                 end,
             },
             sources = {
+                { name = "nvim_lsp" },
                 { name = "buffer" },
                 { name = "path" },
+                { name = "emoji" },
                 { name = "copilot" },
-                { name = "luasnip" },
             },
             view = {
                 entries = { name = "custom", selection_order = "near_cursor" },
