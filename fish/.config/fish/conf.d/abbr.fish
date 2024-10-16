50fdfcde0c8d6e3fd242c68aa4187711d4d51a76
Sander Bongers <sander@bonge.rs>
2024-10-16 14:28:08 +0200

    Rename file & add some abbreviations

---
+++ fish/.config/fish/conf.d/abbr.fish
@@ -0,0 +1,11 @@
+abbr --add bs "brew services"
+abbr --add bc "php bin/console"
+abbr --add ci "composer install"
+abbr --add cu "composer update"
+abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+# Turn gitconfig aliases into Fish abbreviations
+for git_alias in (git config --get-regexp "^alias\." | string replace "alias." "g")
+    echo $git_alias | read --delimiter " " --local alias --local command
+    abbr --add $alias --set-cursor "git $command"
+end
